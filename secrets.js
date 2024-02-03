a9eafddbb9211ffaf19405d0aa7df56e0142fac4
bluesaray12 <saraydefas2019@gmail.com>
2024-02-02 19:27:45 -0500

    Fichero secrets

---
+++ secrets.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const secrets = () => {
+  return (
+    <View>
+      <Text>secrets</Text>
+    </View>
+  )
+}
+
+export default secrets
+
+const styles = StyleSheet.create({})
